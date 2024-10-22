63f257270a0249fb5eb3805d6f3823e9c0121806
Alba <albamrquz3@gmail.com>
2024-10-22 22:38:00 +0200

    documentation expanded

--- src/main/java/com/itacademy/hackaton/controller/DistrictController.java
+++ src/main/java/com/itacademy/hackaton/controller/DistrictController.java
@@ -1,7 +1,15 @@
 package com.itacademy.hackaton.controller;
 
+import com.itacademy.hackaton.entity.IncomeResponseModel;
 import com.itacademy.hackaton.service.DistrictCrudService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -21,6 +29,117 @@ public class DistrictController {
     private final DistrictCrudService districtCrudService;
 
     @Bean
+    @RouterOperations({
+            @RouterOperation(
+                    path = "/api/districts",
+                    operation = @Operation(
+                            summary = "Get all districts",
+                            description = "Returns a list of all districts with their income data",
+                            operationId = "getAllDistricts",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "List of districts",
+                                            content = @Content(
+                                                    mediaType = "application/json",
+                                                    array = @ArraySchema(schema = @Schema(implementation = IncomeResponseModel.class))
+                                            )
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "404",
+                                            description = "No districts found",
+                                            content = @Content(schema = @Schema())
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "500",
+                                            description = "Internal server error",
+                                            content = @Content(schema = @Schema())
+                                    )
+                            }
+                    )
+            ),
+            @RouterOperation(
+                    path = "/api/districts/infantil",
+                    operation = @Operation(
+                            summary = "List of districts with number of pre-school centers",
+                            description = "Returns a list of districts with info about the number of pre-school education centers",
+                            operationId = "getInfantilPercentage",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "List of districts with number of pre-school centers",
+                                            content = @Content(
+                                                    mediaType = "application/json"
+                                            )
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "404",
+                                            description = "No data found for pre-school",
+                                            content = @Content(schema = @Schema())
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "500",
+                                            description = "Internal server error",
+                                            content = @Content(schema = @Schema())
+                                    )
+                            }
+                    )
+            ),
+            @RouterOperation(
+                    path = "/api/districts/primary",
+                    operation = @Operation(
+                            summary = "List of districts with number of primary centers",
+                            description = "Returns a list of districts with info about the number of primary education centers",
+                            operationId = "getPrimaryPercentage",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "List of districts with number of primary centers",
+                                            content = @Content(
+                                                    mediaType = "application/json"
+                                            )
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "404",
+                                            description = "No data found for primary schools",
+                                            content = @Content(schema = @Schema())
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "500",
+                                            description = "Internal server error",
+                                            content = @Content(schema = @Schema())
+                                    )
+                            }
+                    )
+            ),
+            @RouterOperation(
+                    path = "/api/districts/secondary",
+                    operation = @Operation(
+                            summary = "List of districts with number of secondary centers",
+                            description = "Returns a list of districts with info about the number of secondary education centers",
+                            operationId = "getSeccondaryPercentage",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "List of districts with number of secondary centers",
+                                            content = @Content(
+                                                    mediaType = "application/json"
+                                            )
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "404",
+                                            description = "No data found for secondary schools",
+                                            content = @Content(schema = @Schema())
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "500",
+                                            description = "Internal server error",
+                                            content = @Content(schema = @Schema())
+                                    )
+                            }
+                    )
+            )
+    })
     public RouterFunction<ServerResponse> districtRouteFunctions() {
         return route(GET("/api/districts"), this::getAllDistricts)
                 .andRoute(GET("/api/districts/infantil"), this::getInfantilPercentage)
