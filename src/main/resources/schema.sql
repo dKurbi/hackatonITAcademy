cf533f892ea037043ea2635af68131003ff509d9
miguel <miguelsunnata@gmail.com>
2024-10-22 13:11:46 +0200

    Sample csv, schema and connection to h2 database added

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE sample_datos
+(
+
+    Record_Year      INT,          -- Renamed "Year" to "Record_Year"
+    Equipament       VARCHAR(255), -- Changed TEXT to VARCHAR(255)
+    Indicador        VARCHAR(255), -- Changed TEXT to VARCHAR(255)
+    Valor            DOUBLE,
+    Notes_Dades      VARCHAR(255), -- Changed TEXT to VARCHAR(255)
+    Notes_Equipament VARCHAR(255), -- Changed TEXT to VARCHAR(255)
+    Codi_Districte   INT,
+    Titularitat      VARCHAR(255), -- Changed TEXT to VARCHAR(255)
+    TipusGeneral     VARCHAR(255), -- Changed TEXT to VARCHAR(255)
+    TipusEquipament  VARCHAR(255), -- Changed TEXT to VARCHAR(255)
+    Ambit            VARCHAR(255), -- Changed TEXT to VARCHAR(255)
+    Latitud          DOUBLE,
+    Longitud         DOUBLE,
+    Nom_Districte    VARCHAR(255), -- Changed TEXT to VARCHAR(255)
+    Codi_Barri       INT,
+    Nom_Barri        VARCHAR(255)  -- Changed TEXT to VARCHAR(255)
+);
