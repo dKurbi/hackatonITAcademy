5dbc61487d8e5cb2f14be332a1a0201ebfcdaf56
miguel <miguelsunnata@gmail.com>
2024-10-22 15:02:54 +0200

    Typo fixed

--- src/main/java/com/itacademy/hackaton/adapter/controllers/IncomeResponseModel.java
+++ src/main/java/com/itacademy/hackaton/adapter/controllers/IncomeResponseModel.java
@@ -3,7 +3,7 @@ package com.itacademy.hackaton.adapter.controllers;
 public record IncomeResponseModel(
         String name,
         int id,
-        Double calor,
+        Double valor,
         Double index
 ) {
 }
