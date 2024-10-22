b62417c3b9bf6f49223d6a87f0f14ae6b95afb80
Alba <albamrquz3@gmail.com>
2024-10-22 13:26:48 +0200

    district entity

--- src/main/java/com/itacademy/hackaton/adapter/data/entities/District.java
+++ src/main/java/com/itacademy/hackaton/adapter/data/entities/District.java
@@ -1,4 +1,37 @@
 package com.itacademy.hackaton.adapter.data.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("districts")
 public class District {
+
+    @Id
+    private Integer districtId;
+
+    private Integer recordYear;
+    private String equipment;
+    private String indicator;
+    private Double value;
+    private String dataNotes;
+    private String dataEquipment;
+    private String ownership;
+    private String generalType;
+    private String equipmentType;
+    private String scope;
+    private Double latitude;
+    private Double longitude;
+    private String districtName;
+    private Integer neighborhoodCode;
+    private String neighborhoodName;
+
 }
+
